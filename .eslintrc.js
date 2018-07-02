2b6e87fadedfff85d1d5a30d46bf31308e4dafb8
fuzong.wang <fuzong.wang@ikang.com>
2018-07-02 18:50:38 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    "ecmaVersion": 6,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    
+    // 函数名后不接 空格 不提示错误
+    "space-before-function-paren": ["error",{
+      "anonymous": "ignore", 
+      "named": "ignore", 
+      "asyncArrow": "ignore"
+    }]
+  }
+}
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const List = () => import('../views/List.vue')
+const Details = () => import('../views/Details.vue')
+
+export function createRouter() {
+  return new Router({
+    mode: 'history',
+    fallback: false,
+    scrollBehavior: () => ({ y: 0 }),
+    routes: [
+      { path: '/', redirect: '/list' },
+      { path: '/list', component: List },
+      { path: '/details/:id', component: Details }
+    ]
+  })
+}
