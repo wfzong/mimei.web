95997bb16421c997c9ddcf5f9cd9c5bb9ccf4126
fuzong.wang <wfz123@126.com>
2018-07-26 07:50:21 +0800

    update home page

---
+++ src/components/accessories/ImageSection.vue
@@ -0,0 +1,39 @@
+<template>
+    <section-fream section-title="推荐图集">
+        <image-list :imgList="list"></image-list>
+        <magic-button title="查看更多图集" :link="{path:'/list',query:{arttype:'photos'}}"></magic-button>
+    </section-fream>
+</template>
+
+<script>
+/* 
+* 这里处理所有数据，并传递给子组件
+* 子组件接收数据、显示
+*/
+import SectionFream from '@/components/common/SectionFream.vue'
+import ImageList from "@/components/common/ImageList"
+import MagicButton from "@/components/common/MagicButton"
+
+// import axios from 'axios'
+// import * as config from '../../config'
+
+export default {
+    data() {
+        return {
+            imgList:null
+        }
+    },
+    props:['list'],
+    components: {
+        SectionFream,
+        ImageList,
+        MagicButton
+    },
+    created() {
+
+    }
+}
+</script>
+<style lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,30 @@
 <template>
-    <section>
-        <HeaderEle></HeaderEle>
-        {{bigImgList}}
-        <FooterEle></FooterEle>
-    </section>
+    <div :class="{'has-open-menu':MobileMenuOpened}">
+        <header-ele @MenuTrigger="MenuTrigger"></header-ele>
+
+        <image-section :list="bigImgList"></image-section>
+        <!-- <article-and-more-section></article-and-more-section> -->
+
+        <footer-ele/>
+    </div>
 </template>
 <script>
 import HeaderEle from "@/components/Header.vue";
 import FooterEle from "@/components/Footer.vue";
+import ImageSection from "@/components/accessories/ImageSection.vue"
+
 export default {
-    components:{FooterEle, HeaderEle},
+    data() {
+        return {
+        MobileMenuOpened: false
+        };
+    },
+    methods:{
+        MenuTrigger: function(status) {
+        this.MobileMenuOpened = status;
+        }
+    },
+    components:{FooterEle, HeaderEle,ImageSection},
     asyncData({store}){
         return store.dispatch('FETCH_INDEX_DATA')
     },
