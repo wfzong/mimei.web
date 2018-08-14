8e311c83c932adf8f5125118c884deb5d48cc7d0
wfz <wfz123@126.com>
2018-08-14 17:31:34 +0800

    update for launch

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.get(
     }
 )
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8088
 app.listen(port, () => {
   console.log(`server started at localhost:${port}`)
 })
